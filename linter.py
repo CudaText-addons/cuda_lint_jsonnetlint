2b4c1cd65d7058048a797526f12a75a72525994d
Alexey <support@uvviewsoft.com>
2022-10-21 19:39:29 +0300

    init

---
+++ linter.py
@@ -0,0 +1,10 @@
+from cuda_lint import Linter, util
+
+class JSONNetLint(Linter):
+    syntax = 'Jsonnet'
+    name = 'jsonnet-lint'
+    cmd = 'jsonnet-lint'
+    tempfile_suffix = '-'
+    regex = r'(?P<filename>.*):(?P<line>[1-9][0-9]*):(?P<col>[1-9][0-9]*)-(?P<end_col>[1-9][0-9]*) (?P<message>.*)'
+    error_stream = util.STREAM_STDERR
+    multiline = False
---
+++ readme/readme.txt
@@ -0,0 +1,9 @@
+Linter for CudaLint.
+Supports Jsonnet lexer.
+
+Uses "jsonnet-lint" tool, written in Go.
+To install this tool, run in terminal:
+$ go install github.com/google/go-jsonnet/cmd/jsonnet-lint@latest
+
+Ported from SublimeLinter-contrib-jsonnet-lint by Alexey Torgashin (CudaText).
+License: MIT
