09a36f82b51dc691c2aff6ec4cdf2906191df26a
Alexey <support@uvviewsoft.com>
2022-10-22 13:29:18 +0300

    fix

--- linter.py
+++ linter.py
@@ -4,7 +4,7 @@ class JSONNetLint(Linter):
     syntax = 'Jsonnet'
     name = 'jsonnet-lint'
     cmd = 'jsonnet-lint'
-    tempfile_suffix = '-'
+    tempfile_suffix = '.jsonnet'
     regex = r'(?P<filename>.*):(?P<line>[1-9][0-9]*):(?P<col>[1-9][0-9]*)-(?P<end_col>[1-9][0-9]*) (?P<message>.*)'
     error_stream = util.STREAM_STDERR
     multiline = False
